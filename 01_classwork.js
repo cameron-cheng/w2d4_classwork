1f255eaa8a0754e2ab293a38a5cdc6f79813d930
vagrant <Dcameroncheng@gmail.com>
2020-04-09 20:21:31 +0000

    attempted to solve inclass question

--- 01_classwork.js
+++ 01_classwork.js
@@ -1,4 +1,41 @@
 const fs = require('fs');
 
+const allText = [];
+
+fs.readFile(./data/p1.txt, {encoding: 'utf-8'}, function(err,p1){
+  if (!err) {
+      console.log(p1);
+ 
+  } else {
+      console.log(err);
+  }
+});
+fs.readFile(./data/p2.txt, {encoding: 'utf-8'}, function(err,p2){
+  if (!err) {
+      console.log(p2);
+ 
+  } else {
+      console.log(err);
+  }
+});
+fs.readFile(./data/p3.txt, {encoding: 'utf-8'}, function(err,p3){
+  if (!err) {
+      console.log(p3);
+ 
+  } else {
+      console.log(err);
+  }
+});
+fs.readFile(./data/p4.txt, {encoding: 'utf-8'}, function(err,p4){
+  if (!err) {
+      console.log(p4);
+ 
+  } else {
+      console.log(err);
+  }
+});
+
+
+
 // Problem: Read the data files ** in order **, creating one final string as a result
 // The data files are in the ./data directory
